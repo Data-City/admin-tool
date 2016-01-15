aad8111677f48aa08ab01a6d07388c864b6ecef9
Nullmann <nullmann75@gmx.de>
2016-01-15 20:12:05 +0100

    Gruntfile wiederhergestellt

---
+++ Gruntfile.js
@@ -0,0 +1,36 @@
+-'use strict';
+-
+-module.exports = function(grunt) {
+-
+-	grunt.initConfig({
+-	  mongobackup: {
+-		dump : {
+-		  options: {
+-			host : 'localhost',
+-			out : './dumps/mongo'
+-		  }
+-		},
+-		restore: {
+-		  options: {
+-			host : 'localhost',
+-			drop : true,
+-			path : './dumps/mongo/testdb'
+-		  }
+-		}
+-	  }
+-	});
+-  
+-	grunt.loadNpmTasks('mongobackup');
+-
+-  //grunt.loadNpmTasks('grunt-contrib-jshint');
+-  //grunt.loadNpmTasks('grunt-contrib-watch');
+-
+-  // A very basic default task.
+-	grunt.registerTask('default', 'Log some stuff.', function() {
+-		grunt.log.write('Logging some stuff...').ok();
+-		
+-		grunt.task.run([
+-            'mongobackup:dump',
+-        ]);
+-	});
+-}; 
